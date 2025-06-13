8da28a960dde9cf4647ebc3f07e0669b5f711afc
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2025-06-13 14:35:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://pandarestaurantsadder.vercel.app/" || "https://pandarestaurantsuser.vercel.app/",
   credentials: true               
 }));
 
